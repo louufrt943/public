0da6dff01816dd9ab4a35a25e7be6252a9596e74
louufrt943 <louis.fortet943@gmail.com>
2024-04-23 10:25:35 +0200

    nouveau thème enfant

--- wp-content/themes/astra-enfant-planty/functions.php
+++ wp-content/themes/astra-enfant-planty/functions.php
@@ -19,7 +19,7 @@ define( 'CHILD_THEME_ASTRA_ENFANT_PLANTY_VERSION', '1.0.0' );
 function child_enqueue_styles() {
 
 	wp_enqueue_style( 'astra-enfant-planty-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_ENFANT_PLANTY_VERSION, 'all' );
-
+	wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/theme.css')
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
